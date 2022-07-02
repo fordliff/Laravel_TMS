8a950d793e149a2ea129f322a867eb5d498e67f0
fordliff <fordliff@gmail.com>
2022-07-02 12:50:23 +0000

    third changes

--- resources/views/SignIn.blade.php
+++ resources/views/SignIn.blade.php
@@ -17,13 +17,31 @@
            <p class="sign" align="center">Sign In</p>  
               
             <div class="col-md-6">
-               <input class="un " type="text" align="center"  value="{{old('username')}}" placeholder="Username"> 
-               <input class="un " type="text" align="center"  value="{{old('fullname')}}"  placeholder="fullname"> 
-              <input class="un " type="text" align="center"  value="{{old('phoneNo')}}"  placeholder="phoneNo">
+               <input class="un " type="text" align="center" name="username" value="{{old('username')}}" placeholder="Username"> 
+               @error('username')
+                <p style="color:red;">{{$message}}</p>
+               @enderror
+               <input class="un " type="text" align="center"   name="fullname" value="{{old('fullname')}}"  placeholder="Fullname"> 
+               @error('fullname')
+               <p style="color:red;">{{$message}}</p>
+              @enderror
+              <input class="un " type="text" align="center" name="phoneNo"  value="{{old('phoneNo')}}"  placeholder="Phone Number">
+              @error('phoneNo')
+              <p style="color:red;">{{$message}}</p>
+             @enderror
             </div>
-            <div class="col-md-6"> <input class="un " type="text" align="center" value="{{old('email')}}"  placeholder="email">
-              <input class="pass" type="password" align="center"  value="{{'Password'}}" placeholder="Password">
+            <div class="col-md-6"> <input class="un " type="text" align="center" name ="email" value="{{old('email')}}"  placeholder="Email">
+              @error('email')
+              <p style="color:red;">{{$message}}</p>
+             @enderror
+              <input class="pass" type="password" align="center" name="password" value="" placeholder="Password">
+              @error('Password')
+              <p style="color:red;">{{$message}}</p>
+             @enderror
               <input class="pass " type="password" align="center"  name="password_confirmation" value="" placeholder="Confirm Password">
+              @error('password_confirmation')
+              <p style="color:red;">{{$message}}</p>
+             @enderror
             </div>
             <br>
             <div class="col-md-4">
